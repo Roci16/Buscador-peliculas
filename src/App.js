db85fdf123c0868ddb522ae2dfcd539d5e0dd294
rocio ailen <ibrocio16@gmail.com>
2022-02-25 13:55:02 -0300

    ruteo principal en app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@
 const App = () =>{
   return (
     <div>
-      
+       <BrowserRouter>
+              <NavBar/>
+              <Routes>
+            {/* Ruteo de app-movie-jhonhks.herokuapp.com */}
+              <Route path="/" element={<Main/>}/>
+              <Route path="/estrenos" element={<Estrenos/>}/>
+              <Route path="/populares" element={<Populares/>}/>
+              <Route path="/buscar" element={<Buscador/>}/>
+                {/* <Route path="/" element={<Home/>}>Aca va el home</Route>
+                <Route path="/nuevas-peliculas" element={<NuevasPeliculas/>}>despues del home si se hace
+                click en nuevas peliculas</Route>
+                <Route path="/populares" element={<Populares/>}>despues del home sigue eso si se hace click en populares</Route>
+                <Route path="/buscar/search?s=:variableDelImput" element={<Buscar/>}>despues del home sigue 
+                eso si se aprieta busqueda aparese solo search y si se hace una busqueda</Route>
+                <Route path="/movie/:variableNumericaID"> ver pelicula en id</Route>
+                <Route path="/*"> error 404</Route> */}
+              {/* ESTA PAG NO TIENE PAGINADO EN BUSQUEDA Y EL PAGINADO DE POPULARES Y NUEVAS PELIS NO ESTA RUTEADO */}
+              </Routes>
+
+        {/* populares y estrenos pueden ser una variable que diga que en ek onclick que tiene que rellenarse */}
+        {/* en el map va el Link para hacerle click */}
+        {/* links en los botones del nav */}
+        {/* `podian ser lista ul con li en el nav */}
+        </BrowserRouter>
     </div>
   );
 }
