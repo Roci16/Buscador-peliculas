552f9e704b533c18e88e529a5940e414d75aa72a
Roci Ibañez <ibrocio16@gmail.com>
2022-03-18 13:45:58 -0300

    comienzo del footer

---
+++ src/componentes/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>GitHub</li>
+        <li>Linkedin</li>
+        <li>Mail</li>
+      </ul>
+      <span>Hecho con amor</span>
+    </footer>
+  );
+};
+export default Footer;
