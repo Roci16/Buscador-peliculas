0b65e48065ce420f278f3675f3b62856adb016c3
rocio ailen <ibrocio16@gmail.com>
2022-03-25 16:17:01 -0300

    cajas para las tarjetas en home

--- src/componentes/Tarjeta.js
+++ src/componentes/Tarjeta.js
@@ -1,9 +1,37 @@
+import useFetch from "../hooks/useFetch";
+// const peliculas = useFetch("upcoming");
+
+import { Link } from "react-router-dom";
+import Item from "./Item";
+
 import "../style/_tarjeta.scss";
 
-const Tarjeta = () => {
+const Tarjeta = ({ url, titulo }) => {
+  console.log(url);
+
+  const peliculas = useFetch(url);
+  console.log(peliculas);
+  console.log(useFetch(url));
   return (
-    <section className="tarjetas-home">
-      <h3>hola</h3>
+    <section className="">
+      <h2>{titulo}</h2>
+      <article className="tarjetas-home">
+        {peliculas.map((pelicula) => (
+          <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
+            {/* <div>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`}
+                alt={pelicula.title}
+              />
+            </div>
+            <h3>{pelicula.title}</h3> */}
+            {/* <Item
+              title={pelicula.title}
+              image={`https://image.tmdb.org/t/p/original/${pelicula.poster_path}`}
+            /> */}
+          </Link>
+        ))}
+      </article>
     </section>
   );
 };
