46b477c4adf47f675ae6a0bfa9c135cf41f51c1a
Roci Ibañez <ibrocio16@gmail.com>
2022-03-26 19:35:05 -0300

    diseño en tarjetas home

--- src/componentes/Tarjeta.js
+++ src/componentes/Tarjeta.js
@@ -1,22 +1,24 @@
 import useFetch from "../hooks/useFetch";
 
 import { Link } from "react-router-dom";
-// import Item from "./Item";
+import Item from "./Item";
 
 import "../style/_tarjeta.scss";
 
 const Tarjeta = ({ url, titulo }) => {
   const peliculas = useFetch(url);
   return (
-    <section className="">
+    <section className="seccion-home">
       <h2>{titulo}</h2>
       <article className="tarjetas-home">
         {peliculas.map((pelicula) => (
           <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
-            {/* <Item
+            <Item
               title={pelicula.title}
               image={`https://image.tmdb.org/t/p/original/${pelicula.poster_path}`}
-            /> */}
+              styleContainer="conteiner-tarjeta"
+              styleTitle="titulo-tarjeta"
+            />
           </Link>
         ))}
       </article>
