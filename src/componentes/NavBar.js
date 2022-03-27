67167b44452ea40f750a0623b5b144af89add3cb
rocio ailen <ibrocio16@gmail.com>
2022-03-27 20:09:05 -0300

    ruteo de tarjetas

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,34 +1,27 @@
-import {AiFillHome} from "react-icons/ai";
-import {Link} from "react-router-dom"
-import '../style/_navBar.scss'
+import { AiFillHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import "../style/_navBar.scss";
 
-const NavBar = ()=>{
-    return(
-        <nav>
-            <ul>
-                <li className="home">
-                    <Link to="/">
-                        <AiFillHome className="home"/>
-                    </Link>
-                </li>
-                <li className="botones-nav">
-                    <Link to="/populares">
-                        Populares
-                    </Link>
-                </li>
-                <li className="botones-nav">
-                    <Link to="/estrenos" >
-                        Estrenos
-                    </Link>
-                </li>
-                <li className="botones-nav">
-                    <Link to="/buscar">
-                        Buscar
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-       
-    )
-}
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li className="home">
+          <Link to="/">
+            <AiFillHome className="home" />
+          </Link>
+        </li>
+        <li className="botones-nav">
+          <Link to="/populares">Populares</Link>
+        </li>
+        <li className="botones-nav">
+          <Link to="/estrenos">Estrenos</Link>
+        </li>
+        <li className="botones-nav">
+          <Link to="/buscar">Buscar</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default NavBar;
