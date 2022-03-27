8c50381a524091bb9d5b085bf9659d83ef308bd4
rocio ailen <ibrocio16@gmail.com>
2022-03-27 18:29:47 -0300

    estilos a cajas en home

--- src/componentes/Tarjeta.js
+++ src/componentes/Tarjeta.js
@@ -9,15 +9,15 @@ const Tarjeta = ({ url, titulo }) => {
   const peliculas = useFetch(url);
   return (
     <section className="seccion-home">
+      <h2>{titulo}</h2>
       <article className="tarjetas-home">
-        <h2>{titulo}</h2>
         {peliculas.map((pelicula) => (
           <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
             <Item
               title={pelicula.title}
               image={`https://image.tmdb.org/t/p/original/${pelicula.poster_path}`}
               styleContainer="conteiner-tarjeta"
-              // styleTitle="titulo-tarjeta"
+              styleTitle="titulo-tarjeta"
             />
           </Link>
         ))}
