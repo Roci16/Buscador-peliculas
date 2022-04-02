128df9c0bf5a355decd07789edcfb3954967c54d
Roci Ibañez <ibrocio16@gmail.com>
2022-04-02 19:04:38 -0300

    comienzo de detalles

--- src/componentes/DetallePelicula.js
+++ src/componentes/DetallePelicula.js
@@ -1,4 +1,47 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  UrlBase,
+  apiKey,
+  Lenguaje,
+  UrlImagen,
+} from "../auxiliares/VariablesGlobales";
+import "../style/_detalles.scss";
+
 const DetallePelicula = () => {
-  return <h4>Hola</h4>;
+  const params = useParams();
+  const [pelicula, setPelicula] = useState([]);
+  // console.log(params);
+  // console.log(params.idPelicula);
+  useEffect(() => {
+    fetch(`${UrlBase}${params.idPelicula}?${apiKey}${Lenguaje}`)
+      .then((res) => res.json())
+      .then((data) => setPelicula(data));
+  }, [params.idPelicula]);
+  // ${UrlImagen}${pelicula.poster_path}
+  // url(https://placekitten.com/500/)
+  // console.log(setPelicula(data));
+  return (
+    // backdrop_path
+    <section
+      // style={{
+      //   backgroundImage: `url(https://placekitten.com/500)`,
+      // }}
+      style={{
+        backgroundImage: `url(${UrlImagen}${pelicula.backdrop_path})`,
+      }}
+      className="fondo-detalle"
+    >
+      <article>
+        <h2> {pelicula.title}</h2>
+        <div className="container-poster-detalle">
+          <img
+            src={`${UrlImagen}${pelicula.poster_path}`}
+            alt={pelicula.title}
+          />
+        </div>
+      </article>
+    </section>
+  );
 };
 export default DetallePelicula;
