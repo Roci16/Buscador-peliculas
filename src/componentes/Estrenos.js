6f81993050ea4b63e8349957e2bc74d43cb9d8cf
rocio ailen <ibrocio16@gmail.com>
2022-04-01 21:19:17 -0300

    estilos para pc en secciones top/popular/estreno

--- src/componentes/Estrenos.js
+++ src/componentes/Estrenos.js
@@ -1,24 +1,27 @@
 import useFetch from "../hooks/useFetch";
 import { Link } from "react-router-dom";
 import Item from "./Item";
+import "../style/_secciones.scss";
 
 const Estrenos = () => {
   const peliculas = useFetch("upcoming");
 
   return (
-    <div>
+    <section className="seccion-container">
       <h2>Estrenos</h2>
-      {peliculas.map((pelicula) => (
-        <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
-          <Item
-            title={pelicula.title}
-            image={`https://image.tmdb.org/t/p/original/${pelicula.poster_path}`}
-            styleContainer="item-vista-general"
-            styleTitle="titulo-pelicula"
-          />
-        </Link>
-      ))}
-    </div>
+      <section className="coleccion-tarjetas">
+        {peliculas.map((pelicula) => (
+          <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
+            <Item
+              title={pelicula.title}
+              image={`https://image.tmdb.org/t/p/original/${pelicula.poster_path}`}
+              styleContainer="item-vista-general"
+              styleTitle="titulo-pelicula"
+            />
+          </Link>
+        ))}
+      </section>
+    </section>
   );
 };
 export default Estrenos;
