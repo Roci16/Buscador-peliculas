92328f7523f067c8b446fdab8b50246045b04625
rocio ailen <ibrocio16@gmail.com>
2022-04-03 09:07:33 -0300

    adicion de fondo para peliculas que no tienen recurso de imagen

--- src/componentes/DetallePelicula.js
+++ src/componentes/DetallePelicula.js
@@ -7,28 +7,27 @@ import {
   UrlImagen,
 } from "../auxiliares/VariablesGlobales";
 import "../style/_detalles.scss";
+import img from "../img/default.png";
+import cine from "../img/cine.jpg";
 
 const DetallePelicula = () => {
   const params = useParams();
   const [pelicula, setPelicula] = useState([]);
-  // console.log(params);
-  // console.log(params.idPelicula);
+
   useEffect(() => {
     fetch(`${UrlBase}${params.idPelicula}?${apiKey}${Lenguaje}`)
       .then((res) => res.json())
       .then((data) => setPelicula(data));
   }, [params.idPelicula]);
-  // ${UrlImagen}${pelicula.poster_path}
-  // url(https://placekitten.com/500/)
-  // console.log(setPelicula(data));
+
   return (
-    // backdrop_path
     <section
-      // style={{
-      //   backgroundImage: `url(https://placekitten.com/500)`,
-      // }}
       style={{
-        backgroundImage: `url(${UrlImagen}${pelicula.backdrop_path})`,
+        backgroundImage:
+          `url(${UrlImagen}${pelicula.backdrop_path})` ===
+          `url(https://image.tmdb.org/t/p/original/null)`
+            ? `url(${cine})`
+            : `url(${UrlImagen}${pelicula.poster_path})`,
       }}
       className="fondo-detalle"
     >
@@ -36,7 +35,12 @@ const DetallePelicula = () => {
         <h2> {pelicula.title}</h2>
         <div className="container-poster-detalle">
           <img
-            src={`${UrlImagen}${pelicula.poster_path}`}
+            src={
+              `${UrlImagen}${pelicula.poster_path}` ===
+              `https://image.tmdb.org/t/p/original/null`
+                ? `${img}`
+                : `${UrlImagen}${pelicula.poster_path}`
+            }
             alt={pelicula.title}
           />
         </div>
