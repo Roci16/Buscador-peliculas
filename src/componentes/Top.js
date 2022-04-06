421b51406a7681a7df02b2dd231b1e32035f8182
Roci Ibañez <ibrocio16@gmail.com>
2022-04-06 18:12:06 -0300

    adicion de componente cargando en top y populares

--- src/componentes/Top.js
+++ src/componentes/Top.js
@@ -5,6 +5,7 @@ import { UrlImagen } from "../auxiliares/VariablesGlobales";
 import "../style/_secciones.scss";
 import Paginado from "./Paginado";
 import usePaginado from "../hooks/usePaginado";
+import Cargando from "./Cargando";
 
 const Top = () => {
   const {
@@ -16,33 +17,38 @@ const Top = () => {
     handleClickAnterior,
     handleClickSiguiente,
   } = usePaginado();
-  const { peliculas, totalPaginas } = useFetch("top_rated", page);
+  const { peliculas, totalPaginas, cargando } = useFetch("top_rated", page);
 
   return (
     <section className="seccion-container">
-      <h2>Mejores Puntuadas</h2>
-      <section className="coleccion-tarjetas">
-        {peliculas.map((pelicula) => (
-          <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
-            <Item
-              title={pelicula.title}
-              image={`${UrlImagen}${pelicula.poster_path}`}
-              styleContainer="item-vista-general"
-              styleTitle="titulo-pelicula"
-            />
-          </Link>
-        ))}
-      </section>
-      <Paginado
-        handleClickAnterior={handleClickAnterior}
-        handleClickSiguiente={handleClickSiguiente}
-        handleClickSiguienteDoble={handleClickSiguienteDoble}
-        handleClickAnteriorDoble={handleClickAnteriorDoble}
-        handleClickPrimera={handleClickPrimera}
-        handleClickUltima={handleClickUltima}
-        page={page}
-        totalPaginas={totalPaginas}
-      />
+      <Cargando cargando={cargando} />
+      {!cargando && (
+        <>
+          <h2>Mejores Puntuadas</h2>
+          <section className="coleccion-tarjetas">
+            {peliculas.map((pelicula) => (
+              <Link key={pelicula.id} to={`/movie/${pelicula.id}`}>
+                <Item
+                  title={pelicula.title}
+                  image={`${UrlImagen}${pelicula.poster_path}`}
+                  styleContainer="item-vista-general"
+                  styleTitle="titulo-pelicula"
+                />
+              </Link>
+            ))}
+          </section>
+          <Paginado
+            handleClickAnterior={handleClickAnterior}
+            handleClickSiguiente={handleClickSiguiente}
+            handleClickSiguienteDoble={handleClickSiguienteDoble}
+            handleClickAnteriorDoble={handleClickAnteriorDoble}
+            handleClickPrimera={handleClickPrimera}
+            handleClickUltima={handleClickUltima}
+            page={page}
+            totalPaginas={totalPaginas}
+          />
+        </>
+      )}
     </section>
   );
 };
