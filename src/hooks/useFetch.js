5a0edb54b16ea8b70b46b978da3e7821b60b99ef
Roci Ibañez <ibrocio16@gmail.com>
2022-04-06 17:51:29 -0300

    adicion de componente cargando / prueba de componente en estrenos

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,19 +4,23 @@ import { UrlBase, apiKey, Lenguaje } from "../auxiliares/VariablesGlobales";
 const useFetch = (categorias, page) => {
   const [peliculas, setPeliculas] = useState([]);
   const [totalPaginas, setTotalPaginas] = useState(1);
+  const [cargando, setCargando] = useState(false);
 
   useEffect(() => {
+    setCargando(true);
     fetch(`${UrlBase}${categorias}?${apiKey}${Lenguaje}&page=${page}`)
       .then((res) => res.json())
       .then((data) => {
         setPeliculas(data.results);
         setTotalPaginas(data.total_pages);
+        setCargando(false);
       });
   }, [categorias, page]);
 
   return {
     peliculas: peliculas,
     totalPaginas: totalPaginas,
+    cargando: cargando,
   };
 };
 
