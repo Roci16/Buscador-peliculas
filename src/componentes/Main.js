ea2be1b20ba49bc1e90469f1364251884b504a7d
rocio ailen <ibrocio16@gmail.com>
2022-04-06 22:12:42 -0300

    adicion de componente cargando en home

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -1,15 +1,23 @@
 import Carousel from "./Carousel";
 import "../style/_main.scss";
 import Tarjeta from "./Tarjeta";
+import Cargando from "./Cargando";
+import useFetch from "../hooks/useFetch";
 
 const Main = () => {
+  const { peliculas, cargando } = useFetch("now_playing");
   return (
     <main>
-      <Carousel />
-      <section className="box-tarjetas ">
-        <Tarjeta url="popular" titulo="Peliculas Populares" />
-        <Tarjeta url="top_rated" titulo="Peliculas Mejor Puntadas" />
-      </section>
+      <Cargando cargando={cargando} />
+      {!cargando && (
+        <>
+          <Carousel peliculas={peliculas} />
+          <section className="box-tarjetas ">
+            <Tarjeta url="popular" titulo="Peliculas Populares" />
+            <Tarjeta url="top_rated" titulo="Peliculas Mejor Puntadas" />
+          </section>
+        </>
+      )}
     </main>
   );
 };
